0745e92af5e21358bc17da2e7b6652fd1d93831c
es <edgars.sedovs@gmail.com>
2022-01-04 14:27:24 +0000

    Added comments to code (naked.py, migrate_db.py)

--- migrate_db.py
+++ migrate_db.py
@@ -1,3 +1,4 @@
+# Importing all required libraries to run this code
 import logging
 import logging.config
 import mysql.connector
@@ -24,6 +25,7 @@ logger.info('Asteroid DB migration service')
 # Initiating and reading config values
 logger.info('Loading configuration from file')
 
+# Test for errors with try
 try:
 	config = ConfigParser()
 	config.read('config.ini')
@@ -33,32 +35,45 @@ try:
 	mysql_config_mysql_user = config.get('mysql_config', 'mysql_user')
 	mysql_config_mysql_pass = config.get('mysql_config', 'mysql_pass')
 
+# Handle the errors with except
 except:
 	logger.exception('')
 logger.info('DONE')
 
+# Creating connection with DB
 connection = None
 connected = False
 
+# Defines a function to initiate connection with DB
 def init_db():
 	global connection
 	connection = mysql.connector.connect(host=mysql_config_mysql_host, database=mysql_config_mysql_db, user=mysql_config_mysql_user, password=mysql_config_mysql_pass)
 
+# Initiates connection with DB by running the function above
 init_db()
 
+# Defines a function to test connection with the DB
 def get_cursor():
 	global connection
+
+	# Test for errors with try
 	try:
 		connection.ping(reconnect=True, attempts=1, delay=0)
 		connection.commit()
+
+	# Handle the errors with except
 	except mysql.connector.Error as err:
 		logger.error("No connection to db " + str(err))
 		connection = init_db()
 		connection.commit()
+
+	# Will execute as the last task before the try statement completes
 	return connection.cursor()
 
 # Opening connection to mysql DB
 logger.info('Connecting to MySQL DB')
+
+# Test for errors with try
 try:
 	# connection = mysql.connector.connect(host=mysql_config_mysql_host, database=mysql_config_mysql_db, user=mysql_config_mysql_user, password=mysql_config_mysql_pass)
 	cursor = get_cursor()
@@ -70,6 +85,8 @@ try:
 		record = cursor.fetchone()
 		logger.debug('Your connected to - ' + str(record))
 		connection.commit()
+
+# Handle the errors with except
 except Error as e :
 	logger.error('Error while connecting to MySQL' + str(e))
 
@@ -78,83 +95,122 @@ except Error as e :
 def mysql_check_if_table_exists(table_name):
 	records = []
 	cursor = get_cursor()
+
+	# Test for errors with try
 	try:
 		cursor = connection.cursor()
 		result  = cursor.execute("SHOW TABLES LIKE '" + str(table_name) + "'")
 		records = cursor.fetchall()
 		connection.commit()
+
+	# Handle the errors with except
 	except Error as e :
 		logger.error("query: " + "SHOW TABLES LIKE '" + str(table_name) + "'")
 		logger.error('Problem checking if table exists: ' + str(e))
 		pass
+
+	# Will execute as the last task before the try statement completes
 	return records
 
 # Create migrations table
 def mysql_create_migrations_table():
 	cursor = get_cursor()
 	result = []
+
+	# Test for errors with try
 	try:
 		cursor = connection.cursor()
 		result  = cursor.execute( "CREATE TABLE `migrations` ( `id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(255), `exec_ts` INT(10), `exec_dt` varchar(20), PRIMARY KEY (`id`))" )
 		connection.commit()
+
+	# Handle the errors with except
 	except Error as e :
 		logger.error( "CREATE TABLE `migrations` ( `id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(255), `exec_ts` INT(10), `exec_dt` varchar(20), PRIMARY KEY (`id`))" )
 		logger.error('Problem creating migrations table in DB: ' + str(e))
 		pass
+
+	# Will execute as the last task before the try statement completes
 	return result
 
 # Check if table exists
 def mysql_check_if_migration_exists(migration_f_name):
 	records = []
 	cursor = get_cursor()
+
+	# Test for errors with try
 	try:
 		cursor = connection.cursor()
 		result  = cursor.execute("SELECT count(*) FROM migrations WHERE `name` ='" + str(migration_f_name) + "'")
 		records = cursor.fetchall()
 		connection.commit()
+
+	# Handle the errors with except
 	except Error as e :
 		logger.error("SELECT count(*) FROM migrations WHERE `name` ='" + str(migration_f_name) + "'")
 		logger.error('Problem checking if migration exists: ' + str(e))
 		pass
+
+	# Will execute as the last task before the try statement completes
 	return records[0][0]
 
 # Exec any sql on DB
 def mysql_exec_any_sql(sql_query):
 	cursor = get_cursor()
 	status = 0
+
+	# Test for errors with try
 	try:
 		cursor = connection.cursor()
 		result  = cursor.execute( sql_query )
 		logger.info(result)
 		connection.commit()
+
+	# Handle the errors with except
 	except Error as e :
 		logger.error( sql_query )
 		logger.error('Problem executing sql query on DB: ' + str(e))
 		status = 1
 		pass
+
+	# Will execute as the last task before the try statement completes
 	return status
 
 # Migration value insert
 def mysql_migration_value_insert(name, exec_ts, exec_dt):
 	cursor = get_cursor()
+
+	# Test for errors with try
 	try:
 		cursor = connection.cursor()
 		result  = cursor.execute( "INSERT INTO `migrations` (`name`, `exec_ts`, `exec_dt`) VALUES ('" + str(name) + "', '" + str(exec_ts) + "', '" + str(exec_dt) + "')")
 		connection.commit()
+
+	# Handle the errors with except
 	except Error as e :
 		logger.error( "INSERT INTO `migrations` (`name`, `exec_ts`, `exec_dt`) VALUES ('" + str(name) + "', '" + str(exec_ts) + "', '" + str(exec_dt) + "')")
 		logger.error('Problem inserting migration values into DB: ' + str(e))
 		pass
 
+# Testing whether such table exists in the DB
 if mysql_check_if_table_exists("migrations") == []:
+
+	# If does not exist, creates a table according to the predefined function
 	mysql_create_migrations_table()
 else:
+
+	# Otherwise passes information, that such table already exists
 	logger.info("Migrations table exists")
 
+# Creates a new blank array for migration list
 migrations_list = []
+
 # Reading all migration file names into an array
 cur_dir = os. getcwd()
+
+# Lists all files which are in the migration folder
 migrations_files_list = os.listdir(cur_dir + "/migrations/")
+
+# For loop to add all files with .sql extension to the predefined migration list array
 for f_name in migrations_files_list:
 	if f_name.endswith('.sql'):
 		migrations_list.append(f_name)
@@ -162,25 +218,35 @@ for f_name in migrations_files_list:
 # Sorting list to be processed in the correct order
 migrations_list.sort(reverse=False)
 
+# Creates a new variable with value 0 to determine successful and unsuccessful migrations 
 counter = 0
 
+# For loop to go through the migration list
 for migration in migrations_list:
+
+	# If statement to test whether such migration already exists according to the migration name
 	if mysql_check_if_migration_exists(migration) == 0:
+
+		# If such migration does not exist, then opens migration file, reads it contents and executes
 		with open(cur_dir + "/migrations/" + migration,'r') as file:
 			migration_sql = file.read()
 			logger.debug(migration_sql)
 			logger.info("Executing: " + str(migration))
+
+			# If statement to test whether received status code is fine (0) and to write information about the executed migration in the table (to not repeat it twice later)
 			if mysql_exec_any_sql(migration_sql) == 0:
 				mig_exec_ts = int(time.time())
 				mig_exec_dt = datetime.utcfromtimestamp(mig_exec_ts).strftime('%Y-%m-%d %H:%M:%S')
 				mysql_migration_value_insert(migration, mig_exec_ts, mig_exec_dt)
 				logger.info("OK")
 				counter += 1
+
+			# Otherwise, if there are issues when applying migration, prints an error message
 			else:
 				logger.error("Problem applying migration. Aborting")
 				break
 
+# If not migrations exist to execute, prints an error message
 if counter == 0:
 	logger.info("No migrations to execute")	
 
-	
--- naked.py
+++ naked.py
@@ -1,3 +1,4 @@
+# Importing all required libraries to run this code
 import requests
 import json
 import datetime
@@ -5,12 +6,14 @@ import time
 import yaml
 
 from datetime import datetime
+
+# Printing initial message
 print('Asteroid processing service')
 
 # Initiating and reading config values
 print('Loading configuration from file')
 
-# 
+# Individual NASA API key and URL
 nasa_api_key = "D8Gysxwdly9dpyLlWtl5E0ghdIq4KL5BHjJdaKxj"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
@@ -19,62 +22,85 @@ dt = datetime.now()
 request_date = str(dt.year) + "-" + str(dt.month).zfill(2) + "-" + str(dt.day).zfill(2)  
 print("Generated today's date: " + str(request_date))
 
-
+# Requesting info from NASA API
 print("Request url: " + str(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key))
 r = requests.get(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key)
 
+# Printing NASA request response data
 print("Response status code: " + str(r.status_code))
 print("Response headers: " + str(r.headers))
 print("Response content: " + str(r.text))
 
+# If statement based on http status code (200 is successful)
 if r.status_code == 200:
 
+	# Parsing data from json format into two arrays below
 	json_data = json.loads(r.text)
 
+	# Creating arrays for safe and hazardous asteroids
 	ast_safe = []
 	ast_hazardous = []
 
+	# Testing whether such json element exists in the given data
 	if 'element_count' in json_data:
 		ast_count = int(json_data['element_count'])
 		print("Asteroid count today: " + str(ast_count))
 
+		# Testing if there are any asteroids
 		if ast_count > 0:
+
+			# If there are any asteroids, then read the data from the data provided by NASA (URL, name, approach time, size)
 			for val in json_data['near_earth_objects'][request_date]:
+				# If statement to get asteroid characteristics
 				if 'name' and 'nasa_jpl_url' and 'estimated_diameter' and 'is_potentially_hazardous_asteroid' and 'close_approach_data' in val:
+					# Get asteroid name
 					tmp_ast_name = val['name']
+					# Get URL from description
 					tmp_ast_nasa_jpl_url = val['nasa_jpl_url']
+					# Get asteroid diameter if the required information is available (min and max asteroid diameter and km)
 					if 'kilometers' in val['estimated_diameter']:
+						# Testing and defining min and max asteroid diameter if it's available
 						if 'estimated_diameter_min' and 'estimated_diameter_max' in val['estimated_diameter']['kilometers']:
 							tmp_ast_diam_min = round(val['estimated_diameter']['kilometers']['estimated_diameter_min'], 3)
 							tmp_ast_diam_max = round(val['estimated_diameter']['kilometers']['estimated_diameter_max'], 3)
+						# Set a default negative value of min and max asteroid diameter if it is not available
 						else:
 							tmp_ast_diam_min = -2
 							tmp_ast_diam_max = -2
+					# Set a default negative value of the asteroid diameter if it is not available
 					else:
 						tmp_ast_diam_min = -1
 						tmp_ast_diam_max = -1
 
+					# Setting a new text variable
 					tmp_ast_hazardous = val['is_potentially_hazardous_asteroid']
 
+					# If statement if it's a close approach 
 					if len(val['close_approach_data']) > 0:
+						# If these three values exist in data, then setting values for them
 						if 'epoch_date_close_approach' and 'relative_velocity' and 'miss_distance' in val['close_approach_data'][0]:
 							tmp_ast_close_appr_ts = int(val['close_approach_data'][0]['epoch_date_close_approach']/1000)
 							tmp_ast_close_appr_dt_utc = datetime.utcfromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
 							tmp_ast_close_appr_dt = datetime.fromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
 
+							# Checks if speed is available, otherwise sets it to negative value
 							if 'kilometers_per_hour' in val['close_approach_data'][0]['relative_velocity']:
 								tmp_ast_speed = int(float(val['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']))
 							else:
 								tmp_ast_speed = -1
 
+							# Tests if km data is available
 							if 'kilometers' in val['close_approach_data'][0]['miss_distance']:
 								tmp_ast_miss_dist = round(float(val['close_approach_data'][0]['miss_distance']['kilometers']), 3)
 							else:
 								tmp_ast_miss_dist = -1
+						# If those three values mentioned above do not exist in data, then set default values shown below
 						else:
 							tmp_ast_close_appr_ts = -1
 							tmp_ast_close_appr_dt_utc = "1969-12-31 23:59:59"
 							tmp_ast_close_appr_dt = "1969-12-31 23:59:59"
+					
+					# Else statement if it's not a close approach
 					else:
 						print("No close approach data in message")
 						tmp_ast_close_appr_ts = 0
@@ -83,6 +109,7 @@ if r.status_code == 200:
 						tmp_ast_speed = -1
 						tmp_ast_miss_dist = -1
 
+					# Printing information on asteroid characteristics
 					print("------------------------------------------------------- >>")
 					print("Asteroid name: " + str(tmp_ast_name) + " | INFO: " + str(tmp_ast_nasa_jpl_url) + " | Diameter: " + str(tmp_ast_diam_min) + " - " + str(tmp_ast_diam_max) + " km | Hazardous: " + str(tmp_ast_hazardous))
 					print("Close approach TS: " + str(tmp_ast_close_appr_ts) + " | Date/time UTC TZ: " + str(tmp_ast_close_appr_dt_utc) + " | Local TZ: " + str(tmp_ast_close_appr_dt))
@@ -90,27 +117,39 @@ if r.status_code == 200:
 					
 					# Adding asteroid data to the corresponding array
 					if tmp_ast_hazardous == True:
+						# Adding asteroid to hazardous if it is not safe
 						ast_hazardous.append([tmp_ast_name, tmp_ast_nasa_jpl_url, tmp_ast_diam_min, tmp_ast_diam_max, tmp_ast_close_appr_ts, tmp_ast_close_appr_dt_utc, tmp_ast_close_appr_dt, tmp_ast_speed, tmp_ast_miss_dist])
 					else:
+						# Adding asteroid to safe if it is not hazardous
 						ast_safe.append([tmp_ast_name, tmp_ast_nasa_jpl_url, tmp_ast_diam_min, tmp_ast_diam_max, tmp_ast_close_appr_ts, tmp_ast_close_appr_dt_utc, tmp_ast_close_appr_dt, tmp_ast_speed, tmp_ast_miss_dist])
 
+		# If there are no asteroids during this day, then printing the following message
 		else:
 			print("No asteroids are going to hit earth today")
 
+	# Printing a message on the number of safe and hazardous asteroids
 	print("Hazardous asteorids: " + str(len(ast_hazardous)) + " | Safe asteroids: " + str(len(ast_safe)))
 
+	# If statement to perform specific actions if there are and there are not asteroids passing today
 	if len(ast_hazardous) > 0:
 
+		# Sorting asteroids by time from starting from the most recent
 		ast_hazardous.sort(key = lambda x: x[4], reverse=False)
 
+		# Returning a list of asteroids to fly near earth today
 		print("Today's possible apocalypse (asteroid impact on earth) times:")
 		for asteroid in ast_hazardous:
 			print(str(asteroid[6]) + " " + str(asteroid[0]) + " " + " | more info: " + str(asteroid[1]))
 
+		# Sorting asteroids by distance from earth
 		ast_hazardous.sort(key = lambda x: x[8], reverse=False)
+		# Returning information about the closest asteroid to fly near earth
 		print("Closest passing distance is for: " + str(ast_hazardous[0][0]) + " at: " + str(int(ast_hazardous[0][8])) + " km | more info: " + str(ast_hazardous[0][1]))
+
+	# Printing a following message, if there are not asteroids today
 	else:
 		print("No asteroids close passing earth today")
 
+# Printing an error message due to not response from API with a response code and content
 else:
 	print("Unable to get response from API. Response code: " + str(r.status_code) + " | content: " + str(r.text))
